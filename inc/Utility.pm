9c4b0f9cffad799db6069dfe8615527882f8b30d
Kartik Thakore <kthakore@cpan.org>
2009-08-27 11:12:13 +0000

    Import of KTHAKORE/Alien-SDL-0.01 from CPAN.
    gitpan-cpan-distribution: Alien-SDL
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         KTHAKORE/Alien-SDL-0.01.tar.gz
    gitpan-cpan-author:       KTHAKORE
    gitpan-cpan-maturity:     released

---
+++ inc/Utility.pm
@@ -0,0 +1,87 @@
+package inc::Utility;
+use strict;
+use warnings;
+use Carp;
+#use File::chdir;
+use File::Fetch;
+use Archive::Extract;
+
+#checks to see if sdl-config is available
+sub sdl_con_found
+{
+	return 0 if ($^O =~ /MSWin*|Cygwin/);
+	local $_ = 1;	
+	`sdl-config --libs` or $_ = 0;
+	return $_;
+}
+
+
+sub get_url()
+{
+	my $sdl_site = 'http://www.libsdl.org';
+	
+	my $sdl_projects_site =  $sdl_site.'/projects';
+	  
+	my $urls = [
+	 $sdl_site.'/release/SDL-1.2.9.tar.gz',
+	 $sdl_projects_site.'/SDL_image/release/',
+	 $sdl_projects_site.'/SDL_mixer/release/',
+	 $sdl_projects_site.'/SDL_ttf/release/',
+	 $sdl_projects_site.'/SDL_net/release/',
+	];
+	
+	return $urls;
+}
+
+
+#
+sub cleanup_deps_folder {
+        my $dir = shift;
+	local *DIR;
+
+	opendir DIR, $dir or die "opendir $dir: $!";
+	for (readdir DIR) {
+	        next if /^\.{1,2}$/;
+	        my $path = "$dir/$_";
+		unlink $path if -f $path;
+		cleanup_deps_folder($path) if -d $path;
+	}
+	closedir DIR;
+	rmdir $dir or print "error - $!";
+}
+
+#sub get_SDL()
+#{
+	#my $version = shift;
+	#my $suffix = shift;
+#	cleanup_deps_folder('deps');
+#	my $FF = File::Fetch->new( uri =>'http://cloud.github.com/downloads/kthakore/SDL_perl/sdlperl-deps.tar.bz2' );
+#	my $where = $FF->fetch( to => 'deps' );
+#	print "Got archive $where\n";
+#	my $sdl_ar = Archive::Extract->new(archive => $where);
+#	$sdl_ar->extract( to => 'deps' );
+#	
+#	$CWD = 'deps/sdlperl-deps.tar.bz2';
+#	{
+#	`make`;
+#	`make install`;
+#	}
+	#
+#}
+
+sub get_SDL_deps()
+{
+	my $self = shift;
+	my $location = shift;
+	croak "Require a location to extract to $location" if ( !(-d $location) );
+	my $FF = File::Fetch->new( uri =>'http://cloud.github.com/downloads/kthakore/SDL_perl/sdldeps-aug26.zip' );
+	my $where = $FF->fetch( to => $location );
+	print "Got archive $where\n";
+	my $sdl_ar = Archive::Extract->new(archive => $where);
+	$sdl_ar->extract( to => $location );	
+	unlink $where;
+}
+
+
+
+1;
---
+++ lib/Alien/SDL.pm
@@ -0,0 +1,67 @@
+package Alien::SDL;
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+=head1 NAME
+
+Alien::SDL - building, finding and using SDL binaries
+
+=head1 SYNOPSIS
+
+    use Alien::SDL <options>;
+
+    Just gets windows deps for strawberry perl
+=head1 DESCRIPTION
+
+Please see L<Alien> for the manifesto of the Alien namespace.
+
+In short C<Alien::SDL> can be used to detect and get
+configuration settings from an installed SDL and related libraries .
+=cut
+
+sub new
+{
+	my $self = shift;
+}
+
+
+#################### main pod documentation begin ###################
+=head1 BUGS
+Please post issues and bugs at http://github.com/kthakore/Alien_SDL/issues
+
+
+=head1 SUPPORT
+
+
+
+=head1 AUTHOR
+
+    Kartik Thakore
+    CPAN ID: KTHAKORE
+    none
+    Thakore.Kartik@gmail.com
+    http://yapgh.blogspot.com
+
+=head1 COPYRIGHT
+
+This program is free software; you can redistribute
+it and/or modify it under the same terms as Perl itself.
+
+The full text of the license can be found in the
+LICENSE file included with this module.
+
+
+=head1 SEE ALSO
+
+perl(1).
+
+=cut
+
+#################### main pod documentation end ###################
+
+
+1;
+# The preceding line will help the module return a true value
+
